e4cd7d6f1bca4456c47c2d5b91757d1379ccc7a5
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-09-06 01:14:12 +0600

    chore: Create basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,17 @@
-import { useState } from 'react'
-
-import { Button } from 'components/Button'
-
-import viteLogo from '../public/vite.svg'
-
-import reactLogo from './assets/react.svg'
-
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { AboutPage } from 'pages/AboutPage'
+import { CartPage } from 'pages/CartPage'
+import { MainPage } from 'pages/MainPage'
 
 function App() {
-  const [count, setCount] = useState(1)
-
   return (
-    <>
-      <div>
-        <Button>wer</Button>
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vitetest</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path={'/'} element={<MainPage />} />
+      <Route path={'/about/:id'} element={<AboutPage />} />
+      <Route path={'/cart'} element={<CartPage />} />
+
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
   )
 }
 
